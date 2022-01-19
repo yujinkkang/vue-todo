298fde06a9b48bcb7f0d8b890c9fc6d120c0a274
yujinkkang <misoti1012@gmail.com>
2022-01-19 16:51:04 +0900

    todo App 기본틀커밋

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+var my_cmp = {
+  template: '<div>my component</div>'
+}
+
+new Vue({
+  el: '',
+  components:{
+    'my-cmp': my_cmp
+  }
+})
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
